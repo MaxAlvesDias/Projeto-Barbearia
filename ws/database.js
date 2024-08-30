8434ddcf7c0a41f11e95bf0e99c328cdcc843b6c
Maxsuênaldy Alves <maxpnn@hotmail.com>
2024-08-30 16:14:47 -0300

    Arquivos de Rota colaboradors

--- ws/database.js
+++ ws/database.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 // URI do MongoDB com credenciais e nome do banco de dados
-<<<<<<< HEAD
-const URI = 'mongodb+srv://adminUser:Mongodb1007@clusterdev.lf9ma.mongodb.net/Miguel_Barbearia?retryWrites=true&w=majority&appName=Clusterdev';
-=======
 const URI = 'mongodb+srv://adminUser:(senhaDoUsuario)clusterdev.lf9ma.mongodb.net/Miguel_Barbearia?retryWrites=true&w=majority&appName=Clusterdev';
->>>>>>> 25acec6d84f2ed62c1dc734afe3f423710884e53
 
 // Conexão com o banco de dados
 mongoose.connect(URI)
