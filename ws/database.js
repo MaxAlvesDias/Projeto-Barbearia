ce76ea56edcaaf55e2ae257ba28178b55ddbefa7
Maxsuênaldy Alves <maxpnn@hotmail.com>
2024-08-30 17:28:45 -0300

    Remove

--- ws/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// URI do MongoDB com credenciais e nome do banco de dados
-const URI = 'mongodb+srv://adminUser:(senhaDoUsuario)clusterdev.lf9ma.mongodb.net/Miguel_Barbearia?retryWrites=true&w=majority&appName=Clusterdev';
-
-// Conexão com o banco de dados
-mongoose.connect(URI)
-    .then(() => console.log('DB is Up!'))
-    .catch((err) => console.log('Erro na conexão com o DB:', err));
